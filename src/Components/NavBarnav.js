a112f4d6f482b6d12ec031476a31723e0a20dc15
muditsaini98 <mudit.saini1998@gmail.com>
2022-07-09 19:18:59 +0530

    commit2

--- src/Components/NavBarnav.js
+++ src/Components/NavBarnav.js
@@ -10,9 +10,8 @@ const NavBarnav = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              {/* <Nav.Link href="/">Home</Nav.Link> */}
-              <Nav.Link href="/#/teams">Stats By Team</Nav.Link>
-              <Nav.Link href="/#/seasons">Stats By Season</Nav.Link>
+              <Nav.Link href="/teams">Stats By Team</Nav.Link>
+              <Nav.Link href="/seasons">Stats By Season</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
